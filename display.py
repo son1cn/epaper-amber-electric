ddfc180942dfb93b1ddc6a0fd3197ac19255d723
Nelson Gaske <nelson.gaske@gmail.com>
2024-05-15 19:57:20 +0930

    first real update

--- display.py
+++ display.py
@@ -4,8 +4,7 @@
 import amberelectric
 from amberelectric.api import amber_api
 from datetime import datetime, timedelta
-import requests
-from checkamber import amber_acess_token
+import checkamber
 from papirus import PapirusTextPos
 from papirus import Papirus
 
@@ -15,13 +14,13 @@ text=PapirusTextPos(False, rotation = 0)
 text.AddText("1", 0,0 , Id="date", size =35)
 
 #Text Fields
-text.AddText("Cur:", 0,35 , Id="cur", size =100)
-text.AddText("Forecast", 0,125 , Id="forecast", size =25)
-text.AddText("Past", 0,148 , Id="past", size=28)
+text.AddText("Cur:", 50,30 , Id="cur", size =100)
+text.AddText("Forecast", 0,120 , Id="forecast", size =25)
+text.AddText("Past", 0,145 , Id="past", size=28)
 
 # pull amber api token from the checkamber private file
 configuration = amberelectric.Configuration(
-    access_token = amber_access_token
+    access_token = checkamber.grab_token()
 )
 
 # Create an API instance
@@ -64,9 +63,9 @@ current_time = now.strftime("%d.%m %H:%M")
 #update screen with formatting
 text.UpdateText("date", current_time)
 
-text.UpdateText("cur",format(current[0].per_kwh,'3.2f')+u"\u00A2")
-text.UpdateText("forecast","forecast @" + next_hour.strftime('%H:%M') + ": %3.2f" %(current[1].per_kwh))
-text.UpdateText("past", "Last 24Hr: \ncost $%3.2f" %(cost/100) + " kwh %3.2f" %(kwh))
+text.UpdateText("cur",format(current[0].per_kwh,'3.0f')+u"\u00A2")
+text.UpdateText("forecast","@" + next_hour.strftime('%H:%M') + " - %3.0f" %(current[1].per_kwh)+u"\u00A2")
+text.UpdateText("past", "$"+format((cost/100),'3.2f') +" "+format(kwh,'3.2f')+"kwh")
 
 # text.UpdateText("gas","Gas:"+str(gas))
 
